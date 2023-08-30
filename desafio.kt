da352b18c9967c42d152250d0e72edc465db0f50
Jefferson <40587602+jefftdb@users.noreply.github.com>
2023-08-30 18:29:12 -0300

    Editei a classe Nivel, inseri uma variavel nivel a classe ConteudoEducacional, criei uma variavel listInscritos, adicionei 2 métodos a classe formação, alunosDessaFormacao e conteudoDessaFormacao

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,87 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Usuario(val nome: String, val matricula:String, var nota:Int )
 
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, var duracao: Int = 60, val nivel:Nivel)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
+    val listIscritos:List<Usuario> = inscritos
+     
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        
+    }
+    
+    fun alunosDessaFormacao():List<Usuario>{
+        return inscritos
+        
+    }
+    
+    fun conteudoDessaFormacao():List<ConteudoEducacional>{
+        return conteudos
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    // Define o nível dos conteúdos
+   	val basico = Nivel.BASICO
+    val intermediario = Nivel.INTERMEDIARIO
+    val avancado = Nivel.AVANCADO
+   
+    
+    // Cadastra conteúdos
+    
+    val conteudoUm = ConteudoEducacional("Conteudo Um", 30,basico)
+    val conteudoDois = ConteudoEducacional("Conteudo Dois", 45,intermediario)
+    val conteudoTres = ConteudoEducacional("Conteudo Três", 65,avancado)
+    
+    val conteudos: MutableList<ConteudoEducacional> = mutableListOf(conteudoUm,conteudoDois,conteudoTres)
+    
+ 
+    
+       
+    //Cadastra Alunos
+    
+    val jefferson = Usuario("Jefferson","1",10)
+    val neyder = Usuario("Neyder","2",8)
+    val elias = Usuario("Elias","3",5)
+    
+    
+    // Criando a Formação
+    
+    val primeiroSemestre = Formacao("1º Semestre", conteudos)
+    
+    // Matricula os alunos
+    
+    primeiroSemestre.matricular(jefferson)
+    primeiroSemestre.matricular(neyder)
+    primeiroSemestre.matricular(elias)
+    
+    // Vizualiza os alunos cadastrados
+    println(primeiroSemestre.nome)
+    println("Alunos matriculados")
+    println("-----------------------------------")
+    
+    primeiroSemestre.alunosDessaFormacao().forEach {
+        i -> println("Nome: ${i.nome}")
+        println("Matricula: ${i.matricula}")
+        println("Nota: ${i.nota} ")
+        println("-----------------------------------")
+    }
+    
+    
+    println("|----------------Conteudos------------------|")
+    primeiroSemestre.conteudoDessaFormacao().forEach{
+        i -> println("Nome: ${i.nome}")
+        println("Duração: ${i.duracao} horas")
+        println("Nível: ${i.nivel} ")
+        println("-----------------------------------")
+    }
+    
+    
+    
 }
